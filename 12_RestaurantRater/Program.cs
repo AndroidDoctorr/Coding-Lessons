8ddba4fe99c32d2a26deb6f12fc3434fe664b570
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2022-04-04 16:48:09 -0400

    initial

---
+++ 12_RestaurantRater/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//New location for builder in .Net 6 this is where we will add information for our DbContext/Newtonsoft reference loop handling
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//Can
+app.Run("https://localhost:443");
