5bff2327931f4223e30f826ccb5d798d32e4c39e
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2022-04-08 14:42:08 -0400

    Formatting Cleanup

--- 12_RestaurantRaterAPI/Models/RestaurantListItem.cs
+++ 12_RestaurantRaterAPI/Models/RestaurantListItem.cs
@@ -8,10 +8,10 @@ namespace RestaurantRaterAPI.Models
     public class RestaurantListItem
     {
         public int Id { get; set; }
-    public string Name { get; set; }
-    public string Location { get; set; }
-    public double AverageFoodScore { get; set; }
-    public double AverageCleanlinessScore { get; set; }
-    public double AverageAtmosphereScore { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public double AverageFoodScore { get; set; }
+        public double AverageCleanlinessScore { get; set; }
+        public double AverageAtmosphereScore { get; set; }
     }
 }
